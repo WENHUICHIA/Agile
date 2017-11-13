9c86723575fcfae3b138ac31721ab1e75dd0cf87
Mei Huey <Mei Huey@Microsoft>
2017-11-13 11:42:01 +0800

    code for button

--- retrivePending/src/retrievePending.java
+++ retrivePending/src/retrievePending.java
@@ -31,12 +31,18 @@ public class retrievePending extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextAreaResult = new javax.swing.JTextArea();
         jLabelName = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jComboBoxName.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Johnson", "Jackson", "Thomas", "Williams" }));
 
         jButtonCheck.setText("Check");
+        jButtonCheck.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonCheckActionPerformed(evt);
+            }
+        });
 
         jTextAreaResult.setColumns(20);
         jTextAreaResult.setRows(5);
@@ -44,26 +50,31 @@ public class retrievePending extends javax.swing.JFrame {
 
         jLabelName.setText("Select Name to view: ");
 
+        jLabel1.setText("Retrieve Pending Deliveries");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(61, 61, 61)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 703, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabelName)
                         .addGap(18, 18, 18)
                         .addComponent(jComboBoxName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(81, 81, 81)
                         .addComponent(jButtonCheck)))
-                .addContainerGap(333, Short.MAX_VALUE))
+                .addContainerGap(48, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(98, 98, 98)
+                .addGap(51, 51, 51)
+                .addComponent(jLabel1)
+                .addGap(27, 27, 27)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jComboBoxName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButtonCheck)
@@ -76,6 +87,30 @@ public class retrievePending extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButtonCheckActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCheckActionPerformed
+        String name;
+        name = jComboBoxName.getSelectedItem().toString();
+        
+        if (name == "Johnson"){
+            jTextAreaResult.setText("Name:             Address:                                                         ContactNumber:              Order:\n"
+            + "Mary               40,Jalan Bagus,Taman OUG,52000 KL       0125430987                  Fried Chicken (2 boxes)\n"
+            + "Sam               59,Jalan Radin,Taman Bagus,57800 KL     0125439530                  Fried Rice (2 packets)\n");
+            //jTextAreaResult.setText("Sam               59,Jalan Radin,Taman Bagus,57800 KL     0125439530                  Fried Rice (2 packets)");
+        }else if(name == "Jackson"){
+            jTextAreaResult.setText("Name:             Address:                                                                 ContactNumber:              Order:\n"
+            + "James             39,Jalan Indah,Taman Setapak,56000 KL       0120853987                  Chicken Rice (3 packets)\n");
+        }else if(name == "Thomas"){
+            jTextAreaResult.setText("Name:             Address:                                                              ContactNumber:              Order:\n"
+            + "Wilson            20,Jalan Bunga,Taman Bagus,51000 KL     0125492549                  Pizza (2 boxes)\n"
+            + "Taylor              19,Jalan Bunga,Taman Bagus,51000 KL     0125405123                  KFC (4 boxes)\n"
+            + "James            90,Jalan Gembira,Taman Bukit,50800 KL    0168905123                  Fried Chicken (4 boxes)\n");
+        }else if(name == "Williams"){
+            jTextAreaResult.setText("Name:             Address:                                                                 ContactNumber:              Order:\n"
+            + "Smith             49,Jalan Indah,Taman Setapak,56000 KL       0120026518                  Pizza (3 boxes)\n");
+        }
+    }//GEN-LAST:event_jButtonCheckActionPerformed
+
+    
     /**
      * @param args the command line arguments
      */
@@ -114,6 +149,7 @@ public class retrievePending extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonCheck;
     private javax.swing.JComboBox<String> jComboBoxName;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabelName;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextAreaResult;
